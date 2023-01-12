6b2a6a4323c2878b4e42aa31dec812e875a5ae7a
Xiao Xu <u7205634@anu.edu.au>
2023-01-12 22:45:05 +0800

    initialize

---
+++ src/main/java/com/example/campus_management_system/CampusManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.campus_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/campus_management_system/CampusManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
